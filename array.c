4bd9b15e812c49d8d2533c12fe677e58c296f96c
Gourav Baishya <gt3069@srmist.edu.in>
2022-04-24 20:19:01 +0530

    ✨ inital commit

---
+++ array.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ int n,arr[10000],sum=0,i;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        sum=sum+arr[i]; 
+    }
+    printf("%d",sum);
+    return 0;
+}
